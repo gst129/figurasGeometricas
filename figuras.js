50dda4f0cf0e25b39b0dcc1f6a727d6f25b07844
gst129 <gustavoandresborges@gmail.com>
2022-07-17 13:44:35 -0400

    agg cuadrado

--- figuras.js
+++ figuras.js
@@ -1,8 +1,22 @@
 const PI = Math.PI;
 
+diametroCirculo = (radio) => 
+    radio * 2;
+
+console.log("El diametro del circulo es " + diametroCirculo(5));
+
 areaCirculo = (radio) => 
     (radio * radio) * PI;
 
 
+console.log("El area del circulo es: " + areaCirculo(5));
 
-console.log(areaCirculo(5));
+perimetroCuadrado = (ladoC) => 
+     ladoC * 4;
+
+console.log("El perimetro del cuadrado es " + perimetroCuadrado(5));
+
+areaCuadrado = (ladoC) => 
+     ladoC * ladoC;
+
+console.log("El area del cuadrado es: " + areaCuadrado(5));
